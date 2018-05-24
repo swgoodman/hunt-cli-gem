82de6a5f2b87acb52ac2bca536d9c0fccfad7052
“swgoodman” <spencerwgoodman@icloud.com>
2018-05-24 00:31:26 -0700

    Final frame of CLI.

--- lib/product_hunt/cli.rb
+++ lib/product_hunt/cli.rb
@@ -5,6 +5,7 @@ class ProductHunt::CLI
   def call
     greeting
     show_top_five
+    menu
   end
 
   def greeting
@@ -20,6 +21,29 @@ class ProductHunt::CLI
   end
 
   def menu
-    puts "Enter the number of the product you'd like more info on:"
+    input = nil
+    puts "Enter the number of the product you'd like more info on or 'exit':"
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "more info on 1."
+        puts "Type 'list' to choose another or 'exit'."
+      when "2"
+        puts "more info on 2."
+        puts "Type 'list' to choose another or 'exit'."
+      when "list"
+        show_top_five
+        puts "Enter the number of the product you'd like more info on or 'exit':"
+      when "exit"
+        goodbye
+      else
+        puts "Not sure what you want? Type 'list' or 'exit'."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Thanks for stopping by."
   end
 end
