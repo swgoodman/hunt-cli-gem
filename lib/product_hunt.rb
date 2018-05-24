3258e8123d26b2c300973489d4ec57487f6ef542
“swgoodman” <spencerwgoodman@icloud.com>
2018-05-24 00:04:42 -0700

    Established dependencies.

--- lib/product_hunt.rb
+++ lib/product_hunt.rb
@@ -1,4 +1,6 @@
-require "product_hunt/version"
+require_relative "product_hunt/version"
+require_relative "product_hunt/cli"
+require_relative "product_hunt/product"
 
 module ProductHunt
   # Your code goes here...
---
+++ lib/product_hunt/cli.rb
@@ -0,0 +1,14 @@
+
+class ProductHunt::CLI
+
+
+  def call
+    greeting
+  end
+
+  def greeting
+    puts "Welcome to Product Hunt Top Five."
+  end
+
+  
+end
